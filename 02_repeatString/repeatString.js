652d118ea72c345b02a06b9e12ca31427723d2ce
bSkinny2 <117346451+bSkinny2@users.noreply.github.com>
2022-12-02 14:31:12 +0100

    Done ex1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(text, number) {
+    let outputText = text;
+    if (number < 0) {
+        outputText = "ERROR"
+    } else if (number == 0 || text == "") {
+        outputText = ""
+    } else {
+    for (i = 0; i < number - 1; i++) {
+        outputText = outputText + text;
+        }
+    }
+    return outputText;
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
