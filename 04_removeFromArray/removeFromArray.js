2f25f686b196f173a945c3b353248cf2f38c9858
bSkinny2 <117346451+bSkinny2@users.noreply.github.com>
2022-12-04 16:31:38 +0100

    ex4 all tests done!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,25 @@
-const removeFromArray = function(array, par1, par2) {
+const removeFromArray = function(array, par1, par2, par3, par4) {
     let index = array.indexOf(par1);
-    ;
-    array.splice(index, 1);
 
-    if (par2 !== undefined) {
-        let index2 = array.indexOf(par2);
+    if (index !== -1) {
+        array.splice(index, 1);
+        }
+    let index2 = array.indexOf(par2);
+
+    if (index2 !== -1) {
         array.splice(index2, 1);
     }
 
+    let index3 = array.indexOf(par3);
+    if (index3 !== -1) {
+        array.splice(index3, 1);
+    }
+
+    let index4 = array.indexOf(par4);
+    if (index4 !== -1) {
+        array.splice(index4, 1);
+    }
+    
     return array
 };
 
