cb8cb7b0f63de3b217ed6bf810c565369e8e62b1
bSkinny2 <117346451+bSkinny2@users.noreply.github.com>
2022-12-05 12:39:04 +0100

    All tests done.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let minimum = Math.min(...arguments);
+    let maximum = Math.max(...arguments);
+    var output;
+    if (minimum < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        output = "ERROR";
+    } else {
+         output = 0;
+    for (i = minimum; i <= maximum; i++) {
+        output += i;
+    }
+    }
+    return output;
 };
 
 // Do not edit below this line
